ac092387a5e2132c7595e78ef3d826dc6bd2ca7a
FarzXD <110385814+FarzCracking@users.noreply.github.com>
2022-09-26 16:40:25 +0700

    Update 4mbf.py

--- 4mbf.py
+++ 4mbf.py
@@ -867,7 +867,7 @@ def passwrd():
 		for yuzong in id2:
 			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
 			frs = nmf.split(' ')[0]
-			pwv = ['bagong','sayang','kontol','bangsat]
+			pwv = ['bagong','sayang','kontol','bangsat']
 			if len(nmf)<6:
 				if len(frs)<3:
 					pass
